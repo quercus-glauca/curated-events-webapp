5d0a5e459b8c4aba430a8a88fa071c69d6ee24b0
Quercus Glauca <quercusglauca1153@gmail.com>
2022-09-25 19:19:05 +0200

    Step 9-153 Adding a Newsletter API Route

--- components/input/NewsletterRegistration.js
+++ components/input/NewsletterRegistration.js
@@ -1,17 +1,46 @@
+import { useRef, useState } from 'react';
 import classes from './NewsletterRegistration.module.css';
 
 function NewsletterRegistration() {
+  const emailInputRef = useRef();
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [registrationMessage, setRegistrationMessage] = useState();
+
   function registrationHandler(event) {
     event.preventDefault();
 
     // fetch user input (state or refs)
     // optional: validate input
     // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
+    const registrationData = {
+      email: enteredEmail,
+    };
+
+    fetch('/api/registration', {
+      method: 'POST',
+      body: JSON.stringify(registrationData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Registration Data:', data);
+      setRegistrationMessage(data.message);
+      setIsRegistered(true);
+    })
+    .catch((error) => console.log('Registration Error:', error));
+
   }
+
+  const caption = (isRegistered 
+    ? registrationMessage 
+    : 'Sign up to stay updated!');
 
   return (
     <section className={classes.newsletter}>
-      <h2>Sign up to stay updated!</h2>
+      <h2>{caption}</h2>
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
@@ -19,6 +48,7 @@ function NewsletterRegistration() {
             id='email'
             placeholder='Your email'
             aria-label='Your email'
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
---
+++ pages/api/registration.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const registrationData = req.body;
+    res.status(201).json({
+      message: 'You are now registered!',
+      details: `Registration data: ${registrationData.email}`
+    });
+
+  }
+  else {
+    // 405 Method Not Allowed
+    res.status(405).json({message: `${req.method} request received. Try it again using POST.`});
+  }
+}
