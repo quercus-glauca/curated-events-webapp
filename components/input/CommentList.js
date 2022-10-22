7f89dc4af1245169728145e8b844a5eba1df50a8
Quercus Glauca <quercusglauca1153@gmail.com>
2022-10-22 21:48:51 +0200

    Delayed Reload to Allow Reading Notifications

--- components/input/CommentList.js
+++ components/input/CommentList.js
@@ -3,28 +3,36 @@ import { NotificationContext } from '../../context/NotificationProvider';
 import { getUserComments } from '../../data/api-client-fetcher';
 import classes from './CommentList.module.css';
 
+const delayToLoadInSeconds = 4;
+
 function CommentList(props) {
   const { eventId, parentRef, updateList } = props;
   const { showNotification, hideNotification } = useContext(NotificationContext);
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    showNotification('pending', "Comments", "Loading comments...");
-    getUserComments(eventId)
-      .then((result) => {
-        if (result.ok) {
-          hideNotification();
-          setComments(result.essence);
-          parentRef.current.scrollIntoView({ behavior: "smooth" });
-        }
-        else {
-          showNotification('error', "Comments", `${result.essence}`);
-        }
-      })
-      .catch((error) => {
-        const status = error.code || error.errno || error.syscall || 418;
-        showNotification('error', "Comments", `Error (${status}): ${error.message}`);
-      });
+    const timeoutId = setTimeout(() => {
+      showNotification('pending', "Comments", "Loading comments...");
+      getUserComments(eventId)
+        .then((result) => {
+          if (result.ok) {
+            hideNotification();
+            setComments(result.essence);
+            parentRef.current.scrollIntoView({ behavior: "smooth" });
+          }
+          else {
+            showNotification('error', "Comments", `${result.essence}`);
+          }
+        })
+        .catch((error) => {
+          const status = error.code || error.errno || error.syscall || 418;
+          showNotification('error', "Comments", `Error (${status}): ${error.message}`);
+        });
+    }, updateList > 0 ? delayToLoadInSeconds * 1000 : 0);
+
+    return () => {
+      clearTimeout(timeoutId);
+    }
 
   }, [updateList]);
 
